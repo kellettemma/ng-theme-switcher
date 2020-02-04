f38342c67e0f767ee6cfd7bfda27fadb610cb1c3
Emma Kellett <emmakellett@Emmas-MacBook-Air.local>
2020-02-04 19:54:56 +0000

    add comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,20 @@ export class AppComponent {
   isDropdownActive: boolean = true;
   isCodeTheme: boolean;
   
+  /* Toggle Dropdown on side nav */
   toggleDropdown() {
       this.isDropdownActive = !this.isDropdownActive;
   }
 
+  /* Switch to code theme and add class to the html tag */
   switchThemeCode(){
       this.isCodeTheme = true;
       const html = document.querySelector('html');
       html.classList.add('code');
   }
 
-  switchThemeDesign(){
+   /* Switch to design theme and remove class from the html tag */
+   switchThemeDesign(){
     this.isCodeTheme = false;
     const html = document.querySelector('html');
     html.classList.remove('code');
